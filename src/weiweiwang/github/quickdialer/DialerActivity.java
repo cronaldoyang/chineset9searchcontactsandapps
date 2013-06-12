b1477c9e26555defbc344e060920e2bebada107e
cronaldoyang <cronaldo.yang@gmail.com>
2013-06-12 20:18:58 +0800

    change: Rewrite the t9 keyboard layout as AppDialor's keyboard.

--- src/weiweiwang/github/quickdialer/DialerActivity.java
+++ src/weiweiwang/github/quickdialer/DialerActivity.java
@@ -61,21 +61,51 @@ public class DialerActivity extends Activity implements AdapterView.OnItemClickL
 
     private void initView() {
         initSoftKeyboard();
+
         mList = ((Gallery) findViewById(R.id.list));
         mList.setAdapter(mResultAdapter);
         mList.setOnItemClickListener(this);
     }
 
     private void initSoftKeyboard() {
-        ((TextView) findViewById(R.id.btn1)).setText(getKeyboardNumberStr("C", "lear"));
-        ((TextView) findViewById(R.id.btn2)).setText(getKeyboardNumberStr("2", "ABC"));
-        ((TextView) findViewById(R.id.btn3)).setText(getKeyboardNumberStr("3", "DEF"));
-        ((TextView) findViewById(R.id.btn4)).setText(getKeyboardNumberStr("4", "GHI"));
-        ((TextView) findViewById(R.id.btn5)).setText(getKeyboardNumberStr("5", "JKL"));
-        ((TextView) findViewById(R.id.btn6)).setText(getKeyboardNumberStr("6", "MNO"));
-        ((TextView) findViewById(R.id.btn7)).setText(getKeyboardNumberStr("7", "PQRS"));
-        ((TextView) findViewById(R.id.btn8)).setText(getKeyboardNumberStr("8", "TUV"));
-        ((TextView) findViewById(R.id.btn9)).setText(getKeyboardNumberStr("9", "WXYZ"));
+        View clearV = (View) findViewById(R.id.btn_numpad_clear);
+        View num2V = (View) findViewById(R.id.btn_numpad_2);
+        View num3V = (View) findViewById(R.id.btn_numpad_3);
+        View num4V = (View) findViewById(R.id.btn_numpad_4);
+        View num5V = (View) findViewById(R.id.btn_numpad_5);
+        View num6V = (View) findViewById(R.id.btn_numpad_6);
+        View num7V = (View) findViewById(R.id.btn_numpad_7);
+        View num8V = (View) findViewById(R.id.btn_numpad_8);
+        View num9V = (View) findViewById(R.id.btn_numpad_9);
+        setNumpadBtnNumber(num2V, "2", "ABC");
+        setNumpadBtnNumber(num3V, "3", "DEF");
+        setNumpadBtnNumber(num4V, "4", "GHI");
+        setNumpadBtnNumber(num5V, "5", "JKL");
+        setNumpadBtnNumber(num6V, "6", "MNO");
+        setNumpadBtnNumber(num7V, "7", "PQRS");
+        setNumpadBtnNumber(num8V, "8", "TUV");
+        setNumpadBtnNumber(num9V, "9", "WXYZ");
+
+        num2V.setOnClickListener(mBtnClickListener);
+        num3V.setOnClickListener(mBtnClickListener);
+        num4V.setOnClickListener(mBtnClickListener);
+        num5V.setOnClickListener(mBtnClickListener);
+        num6V.setOnClickListener(mBtnClickListener);
+        num7V.setOnClickListener(mBtnClickListener);
+        num8V.setOnClickListener(mBtnClickListener);
+        num9V.setOnClickListener(mBtnClickListener);
+        clearV.setOnClickListener(mBtnClickListener);
+    }
+
+    private void setNumpadBtnNumber(View v, String number, String number_chars) {
+        TextView numberV = (TextView) v.findViewById(R.id.lbl_number);
+        TextView enChars = (TextView) v.findViewById(R.id.lbl_en_chars);
+        numberV.setText(number);
+        enChars.setText(number_chars);
+    }
+
+    private String getNumberByView(View v) {
+        return ((TextView) v.findViewById(R.id.lbl_number)).getText().toString();
     }
 
     private void doAsyncBuild() {
@@ -107,16 +137,20 @@ public class DialerActivity extends Activity implements AdapterView.OnItemClickL
         }
     }
 
-    public void onBtnClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn1:
-                resetIputStr();
-                break;
-            default:
-                buildSearchStr(((TextView) view).getText().toString().substring(0, 1));
-                break;
+    View.OnClickListener mBtnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_numpad_clear:
+                    resetIputStr();
+                    break;
+                default:
+                    buildSearchStr(getNumberByView(view));
+                    break;
+            }
         }
-    }
+    };
 
     private void buildSearchStr(String c) {
         c = c.toLowerCase(Locale.CHINA);
